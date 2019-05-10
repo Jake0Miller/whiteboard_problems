76b0ff6907be892a3cd9384ede74e9d7b47e7c3e
Jake Miller <jake0miller@gmail.com>
2019-05-10 09:42:01 -0600

    Change to puts

--- array_to_hash/array_to_hash.rb
+++ array_to_hash/array_to_hash.rb
@@ -2,4 +2,4 @@ groceries = ["banana", "apple", "orange", "banana", "apple", "apple"]
 temp_hash = Hash.new(0)
 groceries.each { |item| temp_hash[item] += 1 }
 groceries = temp_hash
-print groceries
+puts groceries
