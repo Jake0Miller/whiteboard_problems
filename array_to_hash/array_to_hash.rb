bb70a02b332bfbecf26e7180b8fdbfa7ffacf09e
Jake Miller <jake0miller@gmail.com>
2019-05-10 10:01:34 -0600

    Refactor array_to_hash

--- array_to_hash/array_to_hash.rb
+++ array_to_hash/array_to_hash.rb
@@ -1,5 +1,6 @@
 groceries = ["banana", "apple", "orange", "banana", "apple", "apple"]
-temp_hash = Hash.new(0)
-groceries.each { |item| temp_hash[item] += 1 }
-groceries = temp_hash
-puts groceries
+groceries_hash = Hash.new(0)
+groceries.each { |item| groceries_hash[item] += 1 }
+puts groceries_hash
+groceries_hash = groceries.inject(Hash.new(0)) { |tot, num| tot[num] += 1; tot }
+puts groceries_hash
